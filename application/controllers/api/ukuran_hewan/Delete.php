625c2ca033c250bfd86992c4add7f1ff5fe9e6f3
Vinson Alvindi <45029639+kokoAlvin@users.noreply.github.com>
2020-03-12 14:43:04 +0700

    Update Delete.php

--- application/controllers/api/ukuran_hewan/Delete.php
+++ application/controllers/api/ukuran_hewan/Delete.php
@@ -11,33 +11,33 @@ class Delete extends REST_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Customer_model');
+        $this->load->model('Ukuran_Hewan_model'); 
     }
-    public function index_post($id)
+    public function index_post($id_ukuran_hewan)
     {
 
-        if ($id === null) {
+        if ($id_ukuran_hewan === null) {
             # code...
             $this->response([
                 'status' => false,
-                'message' => 'provide an id not found',
+                'message' => 'GAGAL, ID UKURAN HEWAN TIDAK BOLEH KOSONG !',
 
             ], REST_Controller::HTTP_BAD_REQUEST);
         } else {
-            if ($this->Customer_model->deleteCustomer($id) > 0) {
+            if ($this->Hewan_model->deleteUkuranHewan($id_ukuran_hewan) > 0) {
                 //ok
 
                 $this->response([
                     'status' => true,
-                    'id_customer' => $id,
-                    'message' => 'delete customer Sukses',
+                    'id_hewan' => $id_ukuran_hewan,
+                    'message' => 'SUKSES DELETE UKURAN HEWAN !',
                 ], REST_Controller::HTTP_CREATED);
                 # code...
             } else {
                 ////id not found
                 $this->response([
                     'status' => false,
-                    'message' => 'id tidak ada',
+                    'message' => 'GAGAL DELETE UKURAN HEWAN ID TIDAK DITEMUKAN !',
 
                 ], REST_Controller::HTTP_BAD_REQUEST);
             }
